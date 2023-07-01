f0ce86978cf2f76e851b52fbd57ffb8377c28386
mohamedyessine <92951714+mohamedyessine@users.noreply.github.com>
2023-07-01 16:05:51 +0100

    Update BureauApplication.java

--- back/src/main/java/com/example/bureau/BureauApplication.java
+++ back/src/main/java/com/example/bureau/BureauApplication.java
@@ -11,4 +11,31 @@ public class BureauApplication {
         SpringApplication.run(BureauApplication.class, args);
     }
 
+
+    @Bean
+public CorsFilter corsFilter() {
+   CorsConfiguration corsConfiguration = new CorsConfiguration();
+   corsConfiguration.setAllowCredentials(true);
+   corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5000"));
+   corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+         "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+         "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+   corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+         "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+   corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+   UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+   urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+   return new CorsFilter(urlBasedCorsConfigurationSource);
+}
+
+@Bean
+public CommonsRequestLoggingFilter requestLoggingFilter() {
+   CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+   loggingFilter.setIncludeClientInfo(true);
+   loggingFilter.setIncludeQueryString(true);
+   loggingFilter.setIncludePayload(true);
+   loggingFilter.setMaxPayloadLength(64000);
+   return loggingFilter;
+}
+
 }
